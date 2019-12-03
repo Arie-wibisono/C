019628830f3e1cdca837709ac7a5f8a6ddf87060
Arie-wibisono <it.rsbb@gmail.com>
2019-12-03 15:46:38 +0800

    input

---
+++ input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	//deklarasi variabel
+	char nama[20], umur[3];
+	printf("Input nama anda : ");
+	scanf("%s", &nama);
+
+	printf("Input Umur anda : ");
+        scanf("%s", &umur);
+
+	printf("\n===========================\n");
+	printf("Selamat datang : %s",nama);
+	printf("\nUsia anda sekarang adalah : %s tahun",umur);
+
+	return 0;
+}
