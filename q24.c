3799da83590db49f14cd14b34440e99046dce6e1
Arie-wibisono <it.rsbb@gmail.com>
2020-01-22 09:04:31 +0800

    exercise q2.4

---
+++ q24.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    printf("Wow its a big deal \n");
+    printf("%d \n",22);
+    
+    //way 1
+    printf("The 3 Wise Man \n");
+    //way 2
+    printf("The %d Wise Man \n",3);
+}
